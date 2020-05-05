0880c0a5c5a0d62b40d94e09343eed6a07ccb1e6
Sean Dyon <sean.dyon@outlook.com>
2020-05-05 10:13:01 -0400

    ignore node_modules

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ var express = require("express");
 var app = express();
 
 
-
-
 app.get("/api/user/:username/risk", (req, res, next) => {
     if(req.params.username == "Bob") {
         //console.log(req.params);
@@ -16,7 +14,6 @@ app.get("/api/user/:username/risk", (req, res, next) => {
    });
 
 
-app.listen(3000, () => {
- console.log("Server running on port 3000");
-});
+
+app.listen(process.env.PORT || 3000);
 
